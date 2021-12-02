7d217571162c6bef267b127c665b969d92c3e5b1
pieeri <eric@rubicubix.co.uk>
2021-12-02 10:30:53 +0000

    Simplify CSS dashboard styling

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,32 +2,23 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 import { Box, CssBaseline } from '@material-ui/core';
-import { withStyles, ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 import theme from '../utils/theme';
-
-const styles = (theme) =>({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-});
 
 /**
  * Main dasboard component, initializing a Material UI theme
  * https://mui.com/customization/theming/
  */
-class Dashboard extends Component {
+ export default class Dashboard extends Component {
 
   render() {
-    const { classes, children } = this.props;
-
     return (
       // Enforce a theme on the entire page
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Box className={classes.root}>
-          {children}
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          {this.props.children}
         </Box>
       </ThemeProvider>
     );
@@ -43,5 +34,3 @@ Dashboard.propTypes = {
   /** Can be used to render elements inside the component */
   children: PropTypes.node,
 };
-
-export default withStyles(styles, { withTheme: true })(Dashboard);
