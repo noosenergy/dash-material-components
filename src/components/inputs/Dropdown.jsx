3faef37de54929f0b336d2f8be1d761197ffe4d5
pieeri <eric@rubicubix.co.uk>
2022-04-19 22:36:41 +0100

    Convert selected to actual value in DropDown input

--- src/components/inputs/Dropdown.jsx
+++ src/components/inputs/Dropdown.jsx
@@ -8,19 +8,13 @@ import {
   FormHelperText,
   InputLabel,
   MenuItem,
-  Select,
+  Select
 } from '@material-ui/core';
 
 /**
  * Dropdown component
  */
 export default class Dropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.handleDropdownChange = this.handleDropdownChange.bind(this);
-    this.buildDropdownSelect = this.buildDropdownSelect.bind(this);
-  }
-
   handleDropdownChange = (event) => {
     // Fire Dash-assigned callback
     this.props.setProps({selected: event.target.value});
@@ -29,8 +23,8 @@ export default class Dropdown extends Component {
   buildDropdownSelect = (selected) => {
     return (
       <Box sx={{display: 'flex', flexWrap: 'wrap'}}>
-        {selected.map((i) => (
-          <Chip key={i} label={this.props.options[i]} />
+        {selected.forEach((option, i) => (
+          <Chip key={i} label={option} />
         ))}
       </Box>
     );
@@ -44,6 +38,12 @@ export default class Dropdown extends Component {
     let menuLabel;
     let menuHelper;
     let menuElements = [];
+    const menuControls = {
+      value: selected,
+      multiple: true,
+      onChange: this.handleDropdownChange,
+      renderValue: this.buildDropdownSelect
+    };
 
     // Fetch menu header
     if (labelText) {
@@ -58,7 +58,7 @@ export default class Dropdown extends Component {
     // Fetch menu items
     options.forEach((option, i) => {
       menuElements.push(
-        <MenuItem key={i} value={i}>
+        <MenuItem key={i} value={option}>
           {option}
         </MenuItem>
       );
@@ -69,15 +69,7 @@ export default class Dropdown extends Component {
       <Box id={id} m={2} width={width}>
         <FormControl variant="outlined" fullWidth>
           {menuLabel ? menuLabel : null}
-          <Select
-            labelId={`${id}-label`}
-            label={labelText}
-            id={`${id}-select`}
-            multiple
-            value={selected}
-            onChange={this.handleDropdownChange}
-            renderValue={this.buildDropdownSelect}
-          >
+          <Select labelId={`${id}-label`} label={labelText} id={`${id}-select`} {...menuControls}>
             {menuElements}
           </Select>
           {menuHelper ? menuHelper : null}
@@ -91,7 +83,7 @@ Dropdown.defaultProps = {
   id: 'select',
   width: '100%',
   // No option selected by default
-  selected: [],
+  selected: []
 };
 
 Dropdown.propTypes = {
@@ -113,6 +105,6 @@ Dropdown.propTypes = {
   /** Array of options to select in the dropdown form */
   options: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number])).isRequired,
 
-  /** Active selection indices */
-  selected: PropTypes.arrayOf(PropTypes.number),
+  /** Active option selection */
+  selected: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number]))
 };
