58ad87203fe866e7a641934c9025ea8355bfabe4
pieeri <eric@rubicubix.co.uk>
2022-04-19 21:08:41 +0100

    Ensure multiple MUI app can coexist on page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,9 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import {Box, CssBaseline} from '@material-ui/core';
-import {ThemeProvider} from '@material-ui/core/styles';
+import {Box} from '@material-ui/core';
 
-import theme from '../utils/theme';
+import ThemeProvider from '../utils/ThemeProvider';
 
 /**
  * Main dasboard component, initializing a Material UI theme
@@ -12,13 +11,12 @@ import theme from '../utils/theme';
  */
 export default class Dashboard extends Component {
   render() {
-    const {id, children, height} = this.props;
+    const {id, children, height, cssPrefix} = this.props;
 
     // Enforce and reset a MUI-theme on the entire page
     // And display a dashboard on a full screen width
     return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <ThemeProvider cssPrefix={cssPrefix}>
         <Box id={id} sx={{display: 'flex', flexDirection: 'column', height: height}}>
           {children}
         </Box>
@@ -30,6 +28,7 @@ export default class Dashboard extends Component {
 Dashboard.defaultProps = {
   id: 'dashboard',
   height: '100vh',
+  cssPrefix: 'neptune',
 };
 
 Dashboard.propTypes = {
@@ -41,4 +40,7 @@ Dashboard.propTypes = {
 
   /** Dashboard window height */
   height: PropTypes.string,
+
+  /** CSS class name prefix */
+  cssPrefix: PropTypes.string,
 };
---
+++ src/utils/ThemeProvider.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {CssBaseline} from '@material-ui/core';
+import {
+  createGenerateClassName,
+  StylesProvider,
+  ThemeProvider as MuiThemeProvider,
+} from '@material-ui/core/styles';
+
+import theme from './theme';
+
+const ThemeProvider = ({cssPrefix, children}) => {
+  // Ensure to have a MUI theme with unique CSS class names
+  // (allow several MUI apps loading on the same page)
+  const generateClassName = createGenerateClassName({
+    seed: cssPrefix,
+  });
+
+  // Enforce and reset a MUI-theme on the entire page
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
+    </StylesProvider>
+  );
+};
+
+export default ThemeProvider;
