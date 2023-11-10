48f077280dec1c3913770992df594477108aec1e
pieeri <eric@rubicubix.co.uk>
2023-11-10 17:57:53 +0000

    Apply standard JS structure with tests in-project

--- src/components/inputs/Autocomplete.test.js
+++ src/components/inputs/Autocomplete.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import Autocomplete from '../../../src/components/inputs/Autocomplete';
+import {render, screen, fireEvent} from '@testing-library/react';
+import Autocomplete from './Autocomplete';
 import '@testing-library/jest-dom';
 
 const options = [
-  {'label': 'option_a', 'value': 'value_a'},
-  {'label': 'option_b', 'value': 'value_b'},
-  {'label': 'option_c', 'value': 'value_c'},
+  {label: 'option_a', value: 'value_a'},
+  {label: 'option_b', value: 'value_b'},
+  {label: 'option_c', value: 'value_c'}
 ];
 
 describe('Autocomplete', () => {
@@ -25,7 +25,7 @@ describe('Autocomplete', () => {
   it('should render matching options with inserted input', () => {
     render(<Autocomplete options={options} />);
     const input = screen.getByRole('textbox');
-    fireEvent.change(input, { target: { value: 'option' } });
+    fireEvent.change(input, {target: {value: 'option'}});
     expect(screen.getByText('option_a')).toBeInTheDocument();
     expect(screen.getByText('option_b')).toBeInTheDocument();
     expect(screen.getByText('option_c')).toBeInTheDocument();
@@ -34,16 +34,16 @@ describe('Autocomplete', () => {
   it('should render no options if non-matching input is inserted', () => {
     render(<Autocomplete options={options} />);
     const input = screen.getByRole('textbox');
-    fireEvent.change(input, { target: { value: 'option_d' } });
+    fireEvent.change(input, {target: {value: 'option_d'}});
     expect(screen.queryByText('option_a')).toBeNull();
     expect(screen.queryByText('option_b')).toBeNull();
     expect(screen.queryByText('option_c')).toBeNull();
   });
 
   it('should allow selection by clicking on option', () => {
-    render(<Autocomplete options={options} setProps={() => true}/>);
+    render(<Autocomplete options={options} setProps={() => true} />);
     const input = screen.getByRole('textbox');
-    fireEvent.change(input, { target: { value: 'option' } });
+    fireEvent.change(input, {target: {value: 'option'}});
     fireEvent.click(screen.getByText('option_b'));
     expect(input.value).toBe('option_b');
   });
