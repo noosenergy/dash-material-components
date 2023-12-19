60f01e7eeaf3496ecdef992f2b071222ac042baf
pieeri <eric@rubicubix.co.uk>
2023-12-19 16:56:52 +0000

    Add devcontainer post-create script to provision Dash

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Stop script execution at error
+set -e
+
+# Install js libs
+# ---------------
+echo "$(date) - installing javascript libraries"
+yarn install
+
+# Install py libs
+# ---------------
+echo "$(date) - installing python libraries"
+poetry install --with dev
+
+# Build Dash assets
+# -----------------
+echo "$(date) - building Dash assets"
+yarn build
