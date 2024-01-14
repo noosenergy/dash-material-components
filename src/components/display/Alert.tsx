3aeb5e9c560d439da65377c09d822f8d5300903f
Mattia Primavera <sconqua@gmail.com>
2024-01-14 13:46:02 +0000

    migrate Alert.tsx to ts, keep PropTypes

--- src/components/display/Alert.tsx
+++ src/components/display/Alert.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import {Box, Snackbar, Alert as MuiAlert, AlertColor} from '@mui/material';
-import {DashComponentProps} from '../../props';
-type Props = {
-  /** Used to identify dash components in callbacks */
-  id?: string;
-  /** Alert type */
-  severity?: AlertColor;
-  /** Automatically hide the alert (in ms) */
-  autoHide?: number;
-  /** Message to display */
-  message?: string | null;
-  /** Dash callback to update props on the server */
-  setProps: (props: {message?: string | null}) => void;
-} & DashComponentProps;
+import PropTypes from 'prop-types';
+import {DashComponentProps} from 'props';
 
 /**
  * Alert component
  */
-const Alert: React.FC<Props> = ({
-  id = 'alert',
-  severity = 'error',
-  autoHide = 5000,
-  message = null,
-  setProps
-}) => {
+const Alert = ({id = 'alert', severity = 'error', autoHide = 5000, message, setProps}: Props) => {
   const handleClose = (event: React.SyntheticEvent | Event, reason: string) => {
     if (reason === 'clickaway') {
       setProps({message: null});
@@ -49,12 +32,31 @@ const Alert: React.FC<Props> = ({
   );
 };
 
-Alert.defaultProps = {
-  id: 'alert',
-  severity: 'error',
-  autoHide: 5000,
-  message: null,
-  setProps: () => {}
+type Props = {
+  /** Alert type */
+  severity?: AlertColor;
+  /** Automatically hide the alert (in ms) */
+  autoHide?: number;
+  /** Message to display */
+  message?: string;
+} & DashComponentProps;
+
+Alert.propTypes = {
+  /** Unique ID to identify this component in Dash callbacks. */
+  id: PropTypes.string,
+  /** Alert type */
+  severity: PropTypes.string /*PropTypes.oneOf(['error', 'warning', 'info', 'success'])*/,
+  /** Automatically hide the alert (in ms) */
+  autoHide: PropTypes.number,
+  /** Message to display */
+  message: PropTypes.string,
+  /**
+   * Function to update props to trigger callbacks.
+   * This is a function that takes an object as an argument.
+   */
+  setProps: PropTypes.func.isRequired
 };
+
+Alert.defaultProps = {};
 
 export default Alert;
