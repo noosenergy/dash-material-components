d6b111dc15cb9609c5ba542e1f1b51382491dee8
Mattia Primavera <sconqua@gmail.com>
2024-01-14 14:46:16 +0000

    migrate Section to ts

--- src/components/layout/Section.tsx
+++ src/components/layout/Section.tsx
@@ -3,15 +3,22 @@ import PropTypes from 'prop-types';
 
 import Card from '../../fragments/Card';
 import SectionGrid from '../../fragments/SectionGrid';
+import {DashComponentProps} from 'props';
 
 /**
  * Section component
  * Dashboard > Page > Section
  */
-const Section = (props) => {
-  const {id, children, cards, size, orientation, setProps, downloaded} = props;
-
-  const handleSectionDownload = (event) => {
+const Section = ({
+  id = 'section',
+  children,
+  cards,
+  size,
+  orientation = 'rows',
+  setProps,
+  downloaded = 0
+}: SectionProps) => {
+  const handleSectionDownload = (event: React.MouseEvent) => {
     // Fire Dash-assigned callback
     setProps({downloaded: downloaded + 1});
   };
@@ -48,44 +55,54 @@ const Section = (props) => {
   );
 };
 
-Section.defaultProps = {
-  id: 'section',
-  orientation: 'rows',
-  downloaded: 0
-};
+// TypeScript props type
+type SectionProps = {
+  /** Array of cards to render as component children */
+  cards: Array<{
+    /** Card title */
+    title: string;
+    /** Card size (0 < size <= 12) */
+    size: number;
+    /** Card downloadable */
+    downloadable: boolean;
+  }>;
+  /** Section container size (0 < size <= 12) */
+  size?: number;
+  /** Section general orientation (rows or columns) */
+  orientation?: 'columns' | 'rows';
+  /** Section download counter */
+  downloaded?: number;
+} & DashComponentProps;
 
+// PropTypes for runtime type checking
 Section.propTypes = {
   /** Used to identify dash components in callbacks */
   id: PropTypes.string,
-
   /** Used to enable Dash-assigned component callback */
   setProps: PropTypes.func,
-
   /** Used to render elements inside the component */
   children: PropTypes.node,
-
   /** Array of cards to render as component children */
   cards: PropTypes.arrayOf(
     PropTypes.exact({
       /** Card title */
       title: PropTypes.string,
-
       /** Card size (0 < size <= 12) */
       size: PropTypes.number,
-
-      /** Card dowloadable */
+      /** Card downloadable */
       downloadable: PropTypes.bool
     })
   ),
-
   /** Section container size (0 < size <= 12) */
   size: PropTypes.number,
-
   /** Section general orientation (rows or columns) */
   orientation: PropTypes.oneOf(['columns', 'rows']),
-
   /** Section download counter */
   downloaded: PropTypes.number
 };
+
+// Default
+
+Section.defaultProps = {};
 
 export default Section;
