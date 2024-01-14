baa7be6b9ede50c31c597c6658cd47f395935704
Mattia Primavera <sconqua@gmail.com>
2024-01-14 13:52:28 +0000

    migrate Dashboard component to ts

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import PropTypes from 'prop-types';
-
 import {Box} from '@mui/material';
-
 import ThemeProvider from '../utils/ThemeProvider';
+import {DashComponentProps} from 'props';
 
 /**
- * Main dasboard component, initializing a Material UI theme
+ * Main dashboard component, initializing a Material UI theme
  * https://mui.com/customization/theming/
  */
-const Dashboard = (props) => {
-  const {id, children, height} = props;
-
+const Dashboard = ({id = 'dashboard', children, height = '100vh'}: DashboardProps) => {
   // Enforce and reset a MUI-theme on the entire page
   // And display a dashboard on a full screen width
   return (
@@ -23,20 +20,19 @@ const Dashboard = (props) => {
   );
 };
 
-Dashboard.defaultProps = {
-  id: 'dashboard',
-  height: '100vh'
-};
+type DashboardProps = {
+  id?: string;
+  height?: string;
+} & DashComponentProps;
 
 Dashboard.propTypes = {
-  /** Used to identify dash components in callbacks */
+  /** Unique ID to identify this component in Dash callbacks. */
   id: PropTypes.string,
-
-  /** Used to render elements inside the component */
   children: PropTypes.node,
-
-  /** Dashboard window height */
+  /** Dashboard display height */
   height: PropTypes.string
 };
+
+Dashboard.defaultProps = {};
 
 export default Dashboard;
