bab8c70b6a29237a8e6444c982a653df7ca7be2e
Mattia Primavera <sconqua@gmail.com>
2024-01-14 14:34:28 +0000

    migrate Table component to ts

--- src/components/display/Table.tsx
+++ src/components/display/Table.tsx
@@ -2,17 +2,22 @@ import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {Box, Table as MuiTable, TableContainer, TablePagination} from '@mui/material';
 import {TableBody, TableHead} from '../../fragments/TableContent';
+import {DashComponentProps} from 'props';
 
 /**
  * Table component
  */
-const Table = (props) => {
-  const {id, columns, rowsPerPageOptions, rows: initialRows} = props;
+const Table = ({
+  id = 'table',
+  columns,
+  rows: initialRows,
+  rowsPerPageOptions = [10, 25, 50]
+}: TableProps) => {
   const [rows, setRows] = useState(initialRows);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(rowsPerPageOptions[0]);
 
-  // Make sure state remain in sync with received props
+  // Make sure state remains in sync with received props
   useEffect(() => setRows(initialRows), [initialRows]);
 
   const handlePageChange = (event, value) => {
@@ -45,11 +50,22 @@ const Table = (props) => {
   );
 };
 
-Table.defaultProps = {
-  id: 'table',
-  rowsPerPageOptions: [10, 25, 50]
-};
+// TypeScript props type
+type TableProps = {
+  /** Array of table columns to render */
+  columns: Array<{
+    /** Column field */
+    field: string;
+    /** Column width */
+    width: number;
+  }>;
+  /** Array of table rows to render */
+  rows: Array<object>;
+  /** Table pagination setting */
+  rowsPerPageOptions?: Array<number>;
+} & DashComponentProps;
 
+// PropTypes for runtime type checking
 Table.propTypes = {
   /** Used to identify dash components in callbacks */
   id: PropTypes.string,
@@ -70,5 +86,8 @@ Table.propTypes = {
   /** Table pagination setting */
   rowsPerPageOptions: PropTypes.arrayOf(PropTypes.number)
 };
+
+// Default props
+Table.defaultProps = {};
 
 export default Table;
