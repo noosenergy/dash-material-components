5417eb83779a611c4049746a4a3ba7d82530824e
Mattia Primavera <sconqua@gmail.com>
2024-01-16 02:03:32 +0000

    migrate Button to ts

--- src/components/inputs/Button.tsx
+++ src/components/inputs/Button.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import {Button as MuiButton, Icon, Box} from '@mui/material';
 import PropTypes from 'prop-types';
+import {DashComponentProps} from 'props';
 
 /**
  * Button component
  */
-const Button = (props) => {
-  const {
-    id,
-    text,
-    margin,
-    width,
-    nClicks,
-    startIcon,
-    endIcon,
-    setProps,
-    iconColor,
-    ...remainingProps
-  } = props;
-
+const Button = ({
+  id = 'button',
+  text,
+  margin = 2,
+  width = null,
+  nClicks = 0,
+  startIcon = null,
+  endIcon = null,
+  setProps,
+  iconColor,
+  variant = 'contained',
+  color = 'primary',
+  size = 'medium',
+  disabled = false,
+  disableRipple,
+  disableFocusRipple,
+  disableElevation,
+  href,
+  ...remainingProps
+}: ButtonProps) => {
   const handleClick = (event) => {
     // Fire Dash-assigned callback
     setProps({nClicks: nClicks + 1});
@@ -41,18 +48,41 @@ const Button = (props) => {
   );
 };
 
-Button.defaultProps = {
-  id: 'button',
-  variant: 'contained',
-  color: 'primary',
-  size: 'medium',
-  margin: 2,
-  width: null,
-  startIcon: null,
-  endIcon: null,
-  nClicks: 0,
-  disabled: false
-};
+// TypeScript props type
+type ButtonProps = {
+  /** Button text */
+  text?: string;
+  /** MUI button variant */
+  variant?: 'contained' | 'outlined' | 'text';
+  /** MUI button color */
+  color?: 'primary' | 'secondary' | 'inherit' | 'default';
+  /** Icon color */
+  iconColor?: 'primary' | 'secondary' | 'action' | 'error' | 'disabled';
+  /** MUI button size, small | medium | large */
+  size?: 'small' | 'medium' | 'large';
+  /** Component margin */
+  margin?: number | string;
+  /** Button is disabled */
+  disabled?: boolean;
+  /** Button has no ripple effect */
+  disableRipple?: boolean;
+  /* Disable keyboard focus ripple */
+  disableFocusRipple?: boolean;
+  /** Disable elevation */
+  disableElevation?: boolean;
+  /** Material Icon name to display at start of button */
+  startIcon?: string;
+  /** Material Icon name to display at end of button */
+  endIcon?: string;
+  /** Button link */
+  href?: string;
+  /** Component width */
+  width?: string | null;
+  /** Number of times the button has been clicked */
+  nClicks?: number;
+} & DashComponentProps;
+
+Button.defaultProps = {};
 
 Button.propTypes = {
   /** Component id */
