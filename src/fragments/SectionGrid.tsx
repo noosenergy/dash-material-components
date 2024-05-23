9dbd867c73e112a5c8bda7d29887d390cb0bf315
Mattia Primavera <sconqua@gmail.com>
2024-05-23 12:42:23 +0000

    Align Tabs and graph

--- src/fragments/SectionGrid.tsx
+++ src/fragments/SectionGrid.tsx
@@ -4,14 +4,14 @@ import {DashComponentProps} from 'props';
 import {css} from '@emotion/react';
 
 const sectionInColumnStyle = css`
-  maxheight: 100%;
+  max-height: 100%;
   height: 100%;
   padding: 0;
   margin: 0;
 `;
 
 const sectionInRowStyle = css`
-  maxwidth: 100%;
+  max-width: 100%;
   height: 100%;
   padding: 0;
   margin: 0;
