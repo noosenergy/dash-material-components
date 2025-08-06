c1b9fab7d735da177fdc5f4bec0522e637b6681b
filiperosa <filipe.rosa@tecnico.ulisboa.pt>
2025-08-06 17:14:14 +0100

    Improve webpack config with splitChunks optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,71 @@
 const path = require('path');
-
+const WebpackDashDynamicImport = require('@plotly/webpack-dash-dynamic-import');
+const webpack = require('webpack');
 const packagejson = require('./package.json');
 
 const dashLibraryName = packagejson.name.replace(/-/g, '_');
 
+/**
+ * Webpack configuration for the Dash Material Components library.
+ *
+ * Inspired by dash-core-components/webpack.config.js
+ *
+ * @param {Object} env - Environment variables
+ * @param {Object} argv - Command line arguments
+ * @returns {Object} Webpack configuration
+ */
 module.exports = function (env, argv) {
-  const mode = argv?.mode || 'production';
-  const entry = [path.join(__dirname, 'src/index.ts')];
-  const output = {
-    path: path.join(__dirname, dashLibraryName),
-    filename: `${dashLibraryName}.js`,
-    library: dashLibraryName,
-    libraryTarget: 'umd'
-  };
+  let mode;
 
-  const externals = {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      umd: 'react',
-      root: 'React'
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      umd: 'react-dom',
-      root: 'ReactDOM'
-    }
-  };
+  const overrides = module.exports || {};
+
+  // if user specified mode flag take that value
+  if (argv && argv.mode) {
+    mode = argv.mode;
+  }
+  // else if configuration object is already set (module.exports) use that value
+  else if (overrides.mode) {
+    mode = overrides.mode;
+  }
+  // else take webpack default (production)
+  else {
+    mode = 'production';
+  }
+
+  let filename = (overrides.output || {}).filename;
+  if (!filename) {
+    filename = `${dashLibraryName}.js`;
+  }
+
+  const entry = overrides.entry || {main: './src/index.ts'};
+
+  const externals =
+    'externals' in overrides
+      ? overrides.externals
+      : {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'prop-types': 'PropTypes'
+        };
 
   return {
-    output,
     mode,
     entry,
-    target: 'web',
+    target: ['web', 'es5'],
+    output: {
+      path: path.resolve(__dirname, dashLibraryName),
+      chunkFilename: '[name].js',
+      filename,
+      library: {
+        name: dashLibraryName,
+        type: 'window'
+      }
+    },
     externals,
     resolve: {
       extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
     },
+    // Module rules
     module: {
       rules: [
         {
@@ -75,6 +101,35 @@ module.exports = function (env, argv) {
           ]
         }
       ]
-    }
+    },
+    // Optimization
+    optimization: {
+      splitChunks: {
+        name: '[name].js',
+        cacheGroups: {
+          async: {
+            chunks: 'async',
+            minSize: 0,
+            name(module, chunks, cacheGroupKey) {
+              return `${cacheGroupKey}-${chunks[0].name}`;
+            }
+          },
+          shared: {
+            chunks: 'all',
+            minSize: 0,
+            minChunks: 2,
+            name: `${dashLibraryName}-shared`
+          }
+        }
+      }
+    },
+    // Plugins
+    plugins: [
+      new WebpackDashDynamicImport(),
+      new webpack.SourceMapDevToolPlugin({
+        filename: '[file].map',
+        exclude: ['async-plotlyjs', 'async-mathjax']
+      })
+    ]
   };
 };
