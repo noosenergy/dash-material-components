7a87bff68cf67b39b304d83633e5c9c288f9e884
TheTimb <timothee.brugiere@gmail.com>
2026-04-27 10:17:30 +0000

    Improve other display component

--- src/fragments/Error.tsx
+++ src/fragments/Error.tsx
@@ -1,35 +1,23 @@
 import React from 'react';
 import {Box, Grid, Hidden, Typography} from '@mui/material';
 import {DashComponentProps} from 'props';
-import {css} from '@emotion/react';
 import Page from '../components/layout/Page';
 
-const globalStyle = css({
+const globalStyle = {
   background: 'linear-gradient(180deg, #fac505 30%, #fac505 50%, #e9e9e9 70%, #e9e9e9 100%)',
   height: '100%',
   marginTop: '16px',
   marginRight: '-16px'
-});
+};
 
-const errorLayoutStyle = css({
-  display: 'flex',
-  alignItems: 'center',
-  height: '100%',
-  width: '100%'
-});
-
-const errorStatusLayoutStyle = css({
-  justifyContent: 'right'
-});
-
-const errorMessageLayoutStyle = css({
-  justifyContent: 'left'
-});
+const errorLayoutStyle = {display: 'flex', alignItems: 'center', height: '100%', width: '100%'};
+const errorStatusLayoutStyle = {justifyContent: 'right'};
+const errorMessageLayoutStyle = {justifyContent: 'left'};
 
 const Error = ({id = 'error', status, message}: ErrorProps) => {
   const errorStatus = (
     <Hidden mdDown>
-      <Grid item sm={4} css={[errorLayoutStyle, errorStatusLayoutStyle]}>
+      <Grid item sm={4} sx={[errorLayoutStyle, errorStatusLayoutStyle]}>
         <Typography component="h1" style={{fontWeight: 900, fontSize: '10rem'}}>
           {status}
         </Typography>
@@ -38,7 +26,7 @@ const Error = ({id = 'error', status, message}: ErrorProps) => {
   );
 
   const errorMessage = (
-    <Grid item xs={12} sm={8} css={[errorLayoutStyle, errorMessageLayoutStyle]}>
+    <Grid item xs={12} sm={8} sx={[errorLayoutStyle, errorMessageLayoutStyle]}>
       <Typography component="h1" variant="h1">
         {message}
       </Typography>
@@ -46,7 +34,7 @@ const Error = ({id = 'error', status, message}: ErrorProps) => {
   );
 
   return (
-    <Box id={id} sx={{flexGrow: 1}} css={globalStyle}>
+    <Box id={id} sx={{flexGrow: 1, ...globalStyle}}>
       <Page orientation="columns">
         <Grid container spacing={2} direction="row">
           {errorStatus}
